b5827ee96f8334d34c3cef08461585c820a16bff
glenleedev <glenlee.dev@gmail.com>
2025-09-14 07:45:24 +0700

    chore(db): update seed & schema

--- db-init/app/seed.sql
+++ db-init/app/seed.sql
@@ -2,18 +2,18 @@ INSERT INTO role (name, description) VALUES
 ('admin', 'Allows access to admin and attendance web'),
 ('employee', 'Allows access to attendance web');
 
--- Password example = password123, bcrypt hash
-INSERT INTO user (email, password, role_id) VALUES
-('admin@dexagroup.com', '$2b$10$CwTycUXWue0Thq9StjUM0uJ8vZPzjTnQhP9Jw4rD2oI9x9RlhSyeK', 1),
-('glen@dexagroup.com', '$2b$10$CwTycUXWue0Thq9StjUM0uJ8vZPzjTnQhP9Jw4rD2oI9x9RlhSyeK', 2);
+-- Password example = password, bcrypt hash
+INSERT INTO user (username, password, role_id) VALUES
+('admin@dexagroup.com', '$2a$10$ZsZjmCHwEGqHQl5v/k95WOMrv1.sIJYw6.KtdroUuEduNiSGc3gzC', 1),
+('glen@dexagroup.com', '$2a$10$ZsZjmCHwEGqHQl5v/k95WOMrv1.sIJYw6.KtdroUuEduNiSGc3gzC', 2);
 
 INSERT INTO position (name) VALUES
 ('HR Admin'),
 ('IT Developer');
 
-INSERT INTO employee (user_id, first_name, last_name, position_id, phone, photo) VALUES
-(1, 'Admin', 'HR', 1, '628123456789', NULL),
-(2, 'Glen', '', 2, '628987654321', NULL);
+INSERT INTO employee (user_id, email, first_name, last_name, position_id, phone, photo) VALUES
+(1, 'admin@dexagroup.com', 'Admin', 'HR', 1, '628123456789', NULL),
+(2, 'glen@dexagroup.com', 'Glen', '', 2, '628987654321', NULL);
 
 INSERT INTO attendance_status (name) VALUES
 ('masuk'),
